f0c505ffde7224fe1399328b0d801e22052c3ee6
Masedko <andruugladkuu@gmail.com>
2022-06-12 15:15:12 +0300

    Changes

--- Button.py
+++ Button.py
@@ -1,5 +1,6 @@
 import pygame
 from stuff import font, block_size, upper_margin
+from computer_stuff import screen
 import colors
 
 
@@ -23,21 +24,20 @@ class Button:
     print_message_for_button(): Prints explanatory message next to button
     """
 
-    def __init__(self, x_offset, button_title, message_to_show, screen):
+    def __init__(self, x_offset, button_title, message_to_show, y_offset=10):
         self.__title = button_title
         self.__title_width, self.__title_height = font.size(self.__title)
         self.__message = message_to_show
         self.__button_width = self.__title_width + block_size
         self.__button_height = self.__title_height + block_size
         self.__x_start = x_offset
-        self.__y_start = upper_margin + 10 * block_size + self.__button_height
+        self.__y_start = upper_margin + y_offset * block_size + self.__button_height
         self.rect_for_draw = self.__x_start, self.__y_start, self.__button_width, self.__button_height
         self.rect = pygame.Rect(self.rect_for_draw)
-        self.__rect_for_button_title = (self.__x_start + self.__button_width / 2 -
-                                        self.__title_width / 2, self.__y_start +
-                                        self.__button_height / 2 - self.__title_height / 2)
+        self.__rect_for_button_title = self.__x_start + self.__button_width / 2 - \
+            self.__title_width / 2, self.__y_start + \
+            self.__button_height / 2 - self.__title_height / 2
         self.__color = colors.BLACK
-        self.__screen = screen
 
     def draw_button(self, color=None):
         """
@@ -47,9 +47,9 @@ class Button:
         """
         if not color:
             color = self.__color
-        pygame.draw.rect(self.__screen, color, self.rect_for_draw)
+        pygame.draw.rect(screen, color, self.rect_for_draw)
         text_to_blit = font.render(self.__title, True, colors.WHITE)
-        self.__screen.blit(text_to_blit, self.__rect_for_button_title)
+        screen.blit(text_to_blit, self.__rect_for_button_title)
 
     def change_color_on_hover(self):
         """
@@ -64,10 +64,7 @@ class Button:
         Prints explanatory message next to button
         """
         message_width, message_height = font.size(self.__message)
-        x = self.__x_start / 2 - message_width / 2
-        y = self.__y_start + self.__button_height / 2 - message_height / 2
-        rect_for_message = (x, y)
+        rect_for_message = self.__x_start / 2 - message_width / \
+            2, self.__y_start + self.__button_height / 2 - message_height / 2
         text = font.render(self.__message, True, colors.BLACK)
-        obj = pygame.Rect((x, y), (message_width, message_height))
-        pygame.draw.rect(self.__screen, colors.WHITE, obj)
-        self.__screen.blit(text, rect_for_message)
+        screen.blit(text, rect_for_message)
